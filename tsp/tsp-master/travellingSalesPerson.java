c942e89a65206681b0ac5cc58d55ca71739c6fe4
mihir30 <56292792+mihir30@users.noreply.github.com>
2019-10-20 04:44:32 +0530

    shortest distance found

--- tsp/tsp-master/travellingSalesPerson.java
+++ tsp/tsp-master/travellingSalesPerson.java
@@ -1,60 +1,185 @@
-import java.util.*;
+    import java.util.InputMismatchException;
 
-public class travellingSalesPerson {
-	
-	public static int cost[][], n;
+    import java.util.Scanner;
 
-	public static void read() {
-		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++)
-				cost[i][j] = sc.nextInt();
-			System.out.print("\t");
-		}
-	}
+    import java.util.Stack;
 
-	public static int tsp(int start, int[] tour) {
-		int temp[] = new int[n];
-		int mintour[] = new int[n];
-		int mincost;
-		int ccost;
-		if (start == n - 2)
-			return cost[tour[n - 2]][tour[n - 1]] + cost[tour[n - 1]][0];
-		mincost = 999;
-		for (int i = start + 1; i < n; i++) {
-			for (int j = 0; j < n; j++)
-				temp[j] = tour[j];
-			temp[start + 1] = tour[i];
-			temp[i] = tour[start + 1];
-			if (cost[tour[start]][tour[i]] + (ccost = tsp(start + 1, temp)) < mincost) {
-				mincost = cost[tour[start]][tour[i]] + ccost;
-				for (int k = 0; k < n; k++)
-					mintour[k] = temp[k];
-			}
-		}
-		for (int i = 0; i < n; i++)
-			tour[i] = mintour[i];
-		return mincost;
-	}
+     
 
-	public static void main(String[] args) {
-		System.out.println("\nWELCOME\n\nEnter:-");
-		Scanner sc = new Scanner(System.in);
-		System.out.print(" Number of Cities: ");
-		n = sc.nextInt();
-		cost = new int[n][n];
-		int[] tour = new int[n];
-		System.out.print(" (" + n + " X " + n +") Cost Matrix:\n\t");
-		read();
-		for (int i = 0; i < n; i++)
-			tour[i] = i;
-		int min = tsp(0, tour);
-		System.out.println("\nMinimum Cost: " + min);
-		System.out.print("\nPath:-\n ");
-		for (int i = 0; i < n; i++)
-			System.out.print((tour[i] + 1 ) + " --> ");
-		System.out.println("1");
-	}
-}
+    public class TSPNearestNeighbour
 
+    {
 
+        private int numberOfNodes;
+
+        private Stack<Integer> stack;
+
+     
+
+        public TSPNearestNeighbour()
+
+        {
+
+            stack = new Stack<Integer>();
+
+        }
+
+     
+
+        public void tsp(int adjacencyMatrix[][])
+
+        {
+
+            numberOfNodes = adjacencyMatrix[1].length - 1;
+
+            int[] visited = new int[numberOfNodes + 1];
+
+            visited[1] = 1;
+
+            stack.push(1);
+
+            int element, dst = 0, i;
+
+            int min = Integer.MAX_VALUE;
+
+            boolean minFlag = false;
+
+            System.out.print(1 + "\t");
+
+     
+
+            while (!stack.isEmpty())
+
+            {
+
+                element = stack.peek();
+
+                i = 1;
+
+                min = Integer.MAX_VALUE;
+
+                while (i <= numberOfNodes)
+
+                {
+
+                    if (adjacencyMatrix[element][i] > 1 && visited[i] == 0)
+
+                    {
+
+                        if (min > adjacencyMatrix[element][i])
+
+                        {
+
+                            min = adjacencyMatrix[element][i];
+
+                            dst = i;
+
+                            minFlag = true;
+
+                        }
+
+                    }
+
+                    i++;
+
+                }
+
+                if (minFlag)
+
+                {
+
+                    visited[dst] = 1;
+
+                    stack.push(dst);
+
+                    System.out.print(dst + "\t");
+
+                    minFlag = false;
+
+                    continue;
+
+                }
+
+                stack.pop();
+
+            }
+
+        }
+
+     
+
+        public static void main(String... arg)
+
+        {
+
+            int number_of_nodes;
+
+            Scanner scanner = null;
+
+            try
+
+            {
+
+                System.out.println("Enter the number of nodes in the graph");
+
+                scanner = new Scanner(System.in);
+
+                number_of_nodes = scanner.nextInt();
+
+                int adjacency_matrix[][] = new int[number_of_nodes + 1][number_of_nodes + 1];
+
+                System.out.println("Enter the adjacency matrix");
+
+                for (int i = 1; i <= number_of_nodes; i++)
+
+                {
+
+                    for (int j = 1; j <= number_of_nodes; j++)
+
+                    {
+
+                        adjacency_matrix[i][j] = scanner.nextInt();
+
+                    }
+
+                }
+
+                for (int i = 1; i <= number_of_nodes; i++)
+
+                {
+
+                    for (int j = 1; j <= number_of_nodes; j++)
+
+                    {
+
+                        if (adjacency_matrix[i][j] == 1 && adjacency_matrix[j][i] == 0)
+
+                        {
+
+                            adjacency_matrix[j][i] = 1;
+
+                        }
+
+                    }
+
+                }
+
+                System.out.println("the citys are visited as follows");
+
+                TSPNearestNeighbour tspNearestNeighbour = new TSPNearestNeighbour();
+
+                tspNearestNeighbour.tsp(adjacency_matrix);
+
+            } catch (InputMismatchException inputMismatch)
+
+             {
+
+                 System.out.println("Wrong Input format");
+
+             }
+
+            scanner.close();
+
+        }
+
+    }
